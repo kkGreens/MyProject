6dece1e237af7fef2f4cc66808cd9caad5870c9d
kk <karkuzhaliomrbranch@gmail.com>
2025-03-19 18:12:19 +0530

    third commit

---
+++ src/org/edu/Third.java
@@ -0,0 +1,14 @@
+package org.edu;
+
+public class Third {
+	private void m1() {
+		System.out.println("Tree");
+	}
+	
+	public static void main(String[] args) {
+		
+		Third t = new Third();
+		t.m1();
+	}
+
+}
